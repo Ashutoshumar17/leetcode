a4929a36a13cefa3f17bb1490f730709d3167cd9
Ashutosh kumar <ashutosh.kumar2024a@vitstudent.ac.in>
2026-08-19 10:42:01 +0530

    Time: 27 ms (8.04%), Space: 10.7 MB (93.75%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,34 @@
+int find(vector<int> &a)
+{
+    int maxc = 0;
+    for(int i = 0; i < 256; i++)
+        maxc = max(maxc, a[i]);
+    return maxc;
+}
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        int n = s.size();
+        vector<int> f(256,0);
+
+        int low = 0;
+        int res = 0;
+
+        for(int high = 0; high < n; high++)
+        {
+            f[s[high]]++;
+
+            while((high - low + 1) - find(f) > k)
+            {
+                f[s[low]]--;
+                low++;
+            }
+
+            res = max(res, high - low + 1);
+        }
+
+        return res;
+    }
+};
